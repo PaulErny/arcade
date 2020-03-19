d7260fc75936693ba3fddd9990dc7b545ff818ca
Hugo Lalet <hugo.lalet@epitech.eu>
2020-03-19 14:20:26 +0100

    ADD - method for enterName

--- lib/ILibs.hpp
+++ lib/ILibs.hpp
@@ -94,11 +94,13 @@ class ILibs {
         // closes the window in case of such an event
         virtual void closeWindowEvent(void)=0;
 
-        /* ------------------------------- MENU ------------------------------- */
+         /* ------------------------------- MENU ------------------------------- */
         // menu for selecting the graphical lib. returns 0 if nCurses is selected, 2 for SFML and 3 for SDL -1 for errors
-        int libSelectionMenu(state &pgState, bool close, std::vector<std::string> &libsNames);
+        virtual int libSelectionMenu(state &pgState, bool close, std::vector<std::string> &libsNames)=0;
         // menu to select the game. returns 0 for game 0 if $gamesNames, 1 for game 2 if $gamesNames, etc...
-        int gameSelectionMenu(state &pgState, bool close, std::vector<std::string> &gamesNames, std::vector<std::vector<std::string>> highScores);
+        virtual int gameSelectionMenu(state &pgState, bool close, std::vector<std::string> &gamesNames, std::vector<std::vector<std::string>> highScores)=0;
+        // menu to enter the name. returns string with the name
+        virtual string enterName(state &pgState, bool close)=0;
 
 };
 
