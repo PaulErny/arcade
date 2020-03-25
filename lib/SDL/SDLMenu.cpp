5bc6d484cd26fc66b95b46b018fe483211a87c4b
Paul Erny <paul@MacBook-Pro-de-Paul.home>
2020-03-25 14:11:23 +0100

    same

--- lib/SDL/SDLMenu.cpp
+++ lib/SDL/SDLMenu.cpp
@@ -4,17 +4,17 @@
 // menu for selecting the graphical lib. returns 0 if nCurses is selected, 2 for SFML and 3 for SDL -1 for errors
 int SDLLib::libSelectionMenu(state &pgState, bool close, std::vector<std::string> &libsNames)
 {
-
+    return (-1);
 }
 // menu to select the game. returns 0 for game 0 if $gamesNames, 1 for game 2 if $gamesNames, etc...
 int SDLLib::gameSelectionMenu(state &pgState, bool close, std::vector<std::string> &gamesNames, std::vector<std::vector<std::string>> highScores)
 {
-
+    return (-1);
 }
 // menu to enter the name. returns string with the name
 std::string SDLLib::enterName(state &pgState, bool close)
 {
-
+    return ("");
 }
 
 void SDLLib::eventMenu(int &indexLib)
