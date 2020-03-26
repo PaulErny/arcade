a3560325b53848a5b60b0b7920aae54d2936abeb
Paul Erny <paul@MacBook-Pro-de-Paul.home>
2020-03-26 17:13:19 +0100

    core upgrade, now ready for games to be implemented

--- core/main.cpp
+++ core/main.cpp
@@ -11,8 +11,8 @@ int main(int ac, char *av[])
         if (ac != 2)
             throw "Arcade accept 2 arguments";
         Core core(av[1]);
-        core.laodLib();
-        core.fillLibVector();
+        core.run();
+        //core.fillLibVector();
     }
     catch (const std::exception &e) {
         std::cerr << e.what() << std::endl;
