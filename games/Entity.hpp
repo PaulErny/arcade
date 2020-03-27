2f20b15a3a894537aa8e9620628480971088abd5
victor <victor.maurin@epitech.eu>
2020-03-27 16:45:28 +0100

    entity Igames

---
+++ games/Entity.hpp
@@ -0,0 +1,13 @@
+#ifndef IENTITY_HPP
+#define IENTITY_HPP
+
+class IEntity
+{
+private:
+    /* data */
+public:
+    IEntity(/* args */);
+    ~IEntity();
+};
+
+#endif
---
+++ games/IGames.hpp
@@ -0,0 +1,12 @@
+#ifndef IGAMES_HPP
+#define IGAMES_HPP
+
+class Igames
+{
+private:
+    /* data */
+public:
+    virtual ~Igames()=0;
+};
+
+#endif
