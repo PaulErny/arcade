caf68c128f17436d44d1dcd12225a9403020f6db
Paul Erny <paul@macbook-pro-de-paul.home>
2020-04-01 19:08:48 +0200

    segfaults due to pacman map fixed + textures almost properly assigned (exept for inward angles)

--- games/pacman/Pacman.cpp
+++ games/pacman/Pacman.cpp
@@ -12,33 +12,33 @@ Pacman::Pacman()
 {
     this->map = {{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, // 0 = empty -> black
                  {1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1}, // 1 = wall
-                 {1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1}, // 2 = small coin
-                 {1, 3, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 3, 1}, // 3 = big coin
-                 {1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1}, // 4 = unaccessible for player
-                 {1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1},
-                 {1, 2, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 2, 1},
-                 {1, 2, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 2, 1},
-                 {1, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 1},
-                 {1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1},
-                 {4, 4, 4, 4, 4, 1, 2, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 2, 1, 4, 4, 4, 4, 4},
+                 {1, 2, 5, 1, 1, 6, 2, 5, 1, 1, 1, 6, 2, 1, 1, 2, 5, 1, 1, 1, 6, 2, 5, 1, 1, 6, 2, 1}, 
+                 {1, 3, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 3, 1}, // 5 = angle 
+                 {1, 2, 7, 1, 1, 8, 2, 7, 1, 1, 1, 8, 2, 7, 8, 2, 7, 1, 1, 1, 8, 2, 7, 1, 1, 8, 2, 1}, // 6 = angle 
+                 {1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1}, // 7 = angle
+                 {1, 2, 5, 1, 1, 6, 2, 5, 6, 2, 5, 1, 1, 1, 1, 1, 1, 6, 2, 5, 6, 2, 5, 1, 1, 6, 2, 1}, // 8 = angle
+                 {1, 2, 7, 1, 1, 8, 2, 1, 1, 2, 7, 1, 1, 6, 5, 1, 1, 8, 2, 1, 1, 2, 7, 1, 1, 8, 2, 1}, 
+                 {1, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 1}, // 2 = small coin
+                 {1, 1, 1, 1, 1, 1, 2, 1, 7, 1, 1, 6, 0, 1, 1, 0, 5, 1, 1, 8, 1, 2, 1, 1, 1, 1, 1, 1}, // 3 = big coin
+                 {4, 4, 4, 4, 4, 1, 2, 1, 5, 1, 1, 8, 0, 7, 8, 0, 7, 1, 1, 6, 1, 2, 1, 4, 4, 4, 4, 4}, // 4 = unaccessible for player
                  {4, 4, 4, 4, 4, 1, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 1, 4, 4, 4, 4, 4},
                  {4, 4, 4, 4, 4, 1, 2, 1, 1, 0, 1, 1, 1, 4, 4, 1, 1, 1, 0, 1, 1, 2, 1, 4, 4, 4, 4, 4},
-                 {1, 1, 1, 1, 1, 1, 2, 1, 1, 0, 1, 4, 4, 4, 4, 4, 4, 1, 0, 1, 1, 2, 1, 1, 1, 1, 1, 1},
+                 {1, 1, 1, 1, 1, 1, 2, 7, 8, 0, 1, 4, 4, 4, 4, 4, 4, 1, 0, 7, 8, 2, 1, 1, 1, 1, 1, 1},
                  {0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 1, 4, 4, 4, 4, 4, 4, 1, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0},
-                 {1, 1, 1, 1, 1, 1, 2, 1, 1, 0, 1, 4, 4, 4, 4, 4, 4, 1, 0, 1, 1, 2, 1, 1, 1, 1, 1, 1},
+                 {1, 1, 1, 1, 1, 1, 2, 5, 6, 0, 1, 4, 4, 4, 4, 4, 4, 1, 0, 5, 6, 2, 1, 1, 1, 1, 1, 1},
                  {4, 4, 4, 4, 4, 1, 2, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 2, 1, 4, 4, 4, 4, 4},
                  {4, 4, 4, 4, 4, 1, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 1, 4, 4, 4, 4, 4},
-                 {4, 4, 4, 4, 4, 1, 2, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 2, 1, 4, 4, 4, 4, 4},
-                 {1, 1, 1, 1, 1, 1, 2, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 2, 1, 1, 1, 1, 1, 1},
+                 {4, 4, 4, 4, 4, 1, 2, 1, 1, 0, 5, 1, 1, 1, 1, 1, 1, 6, 0, 1, 1, 2, 1, 4, 4, 4, 4, 4},
+                 {1, 1, 1, 1, 1, 1, 2, 7, 8, 0, 7, 1, 1, 6, 5, 1, 1, 8, 0, 7, 8, 2, 1, 1, 1, 1, 1, 1},
                  {1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1},
-                 {1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1},
-                 {1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1},
-                 {1, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 1},
-                 {1, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 1},
-                 {1, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 1},
+                 {1, 2, 5, 1, 1, 6, 2, 5, 1, 1, 1, 6, 2, 1, 1, 2, 5, 1, 1, 1, 6, 2, 5, 1, 1, 6, 2, 1},
+                 {1, 2, 7, 1, 6, 1, 2, 7, 1, 1, 1, 8, 2, 7, 8, 2, 7, 1, 1, 1, 8, 2, 1, 5, 1, 8, 2, 1},
+                 {1, 3, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 3, 1},
+                 {1, 1, 6, 2, 1, 1, 2, 5, 6, 2, 5, 1, 1, 1, 1, 1, 1, 6, 2, 5, 6, 2, 1, 1, 2, 5, 1, 1},
+                 {1, 1, 8, 2, 7, 8, 2, 1, 1, 2, 7, 1, 1, 6, 5, 1, 1, 8, 2, 1, 1, 2, 7, 8, 2, 7, 1, 1},
                  {1, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 1},
-                 {1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1},
-                 {1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1},
+                 {1, 2, 5, 1, 1, 1, 1, 8, 7, 1, 1, 6, 2, 1, 1, 2, 5, 1, 1, 8, 7, 1, 1, 1, 1, 6, 2, 1},
+                 {1, 2, 7, 1, 1, 1, 1, 1, 1, 1, 1, 8, 2, 7, 8, 2, 7, 1, 1, 1, 1, 1, 1, 1, 1, 8, 2, 1},
                  {1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1},
                  {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}};
 
@@ -59,7 +59,7 @@ void Pacman::initGameData()
     for (size_t y = 0; y < this->map.size(); y++) {
         for (size_t x = 0; x < this->map[y].size(); x++) {
             this->mapSpritesID[y][x].setLibPtr(this->graphics);
-            this->mapSpritesID[y][x].setPosition(x*32, y*32);
+            this->mapSpritesID[y][x].setPosition(92 + x*32, 44 + y*32);
         }
     }
 }
@@ -79,22 +79,16 @@ void Pacman::initMapCell(int x, int y)
 {
     if (y > 0 && y < this->map.size() && x > 0 && x < this->map[0].size() && !this->isAdjacentCellForbidden(x, y)) {
         this->mapSpritesID[y][x].setType(SPRITE);
-        if (this->map[y - 1][x] == 1 && this->map[y][x - 1] == 1 && this->map[y + 1][x] != 1 && this->map[y][x + 1] != 1)
-            this->mapSpritesID[y][x].addImage("pacman/resources/turn_down_left.png");
-        else if (this->map[y - 1][x] == 1 && this->map[y][x + 1] == 1 && this->map[y + 1][x] != 1 && this->map[y][x - 1] != 1)
-            this->mapSpritesID[y][x].addImage("pacman/resources/turn_down_right.png");
-        else if (this->map[y + 1][x] == 1 && this->map[y][x - 1] == 1 && this->map[y - 1][x] != 1 && this->map[y][x + 1] != 1)
-            this->mapSpritesID[y][x].addImage("pacman/resources/turn_up_left.png");
-        else if (this->map[y + 1][x] == 1 && this->map[y][x + 1] == 1 && this->map[y - 1][x] != 1 && this->map[y][x - 1] != 1)
-            this->mapSpritesID[y][x].addImage("pacman/resources/turn_up_right.png");
-        else if (this->map[y - 1][x] == 2 || this->map[y - 1][x] == 3)
-            this->mapSpritesID[y][x].addImage("pacman/resources/wall_horizontal_down.png");
-        else if (this->map[y + 1][x] == 2 || this->map[y + 1][x] == 3)
+        if (this->map[y - 1][x] == 1 && this->map[y + 1][x] == 1 && this->map[y][x - 1] == 1 && this->map[y][x + 1] == 1)
+            this->mapSpritesID[y][x].addImage("pacman/resources/empty.png");
+        else if (this->map[y - 1][x] == 2 || this->map[y - 1][x] == 3 || this->map[y - 1][x] == 0)
             this->mapSpritesID[y][x].addImage("pacman/resources/wall_horizontal_up.png");
-        else if (this->map[y][x - 1] == 2 || this->map[y][x - 1] == 3)
-            this->mapSpritesID[y][x].addImage("pacman/resources/wall_vertical_right.png");
-        else if (this->map[y][x + 1] == 2 || this->map[y][x + 1] == 3)
+        else if (this->map[y + 1][x] == 2 || this->map[y + 1][x] == 3 || this->map[y + 1][x] == 0)
+            this->mapSpritesID[y][x].addImage("pacman/resources/wall_horizontal_down.png");
+        else if (this->map[y][x - 1] == 2 || this->map[y][x - 1] == 3 || this->map[y][x - 1] == 0)
             this->mapSpritesID[y][x].addImage("pacman/resources/wall_vertical_left.png");
+        else if (this->map[y][x + 1] == 2 || this->map[y][x + 1] == 3 || this->map[y][x + 1] == 0)
+            this->mapSpritesID[y][x].addImage("pacman/resources/wall_vertical_right.png");
     } else {
         this->mapSpritesID[y][x].setType(SHAPE);
         this->mapSpritesID[y][x].addShape(32, 32, 30, 30, 230);
@@ -107,6 +101,18 @@ void Pacman::initGraphics()
         for (size_t x = 0; x < this->map[y].size(); x++) {
             if (this->map[y][x] == 1) {
                 this->initMapCell(x, y);
+            } else if (this->map[y][x] == 5) {
+                this->mapSpritesID[y][x].setType(SPRITE);
+                this->mapSpritesID[y][x].addImage("pacman/resources/turn_up_right.png");
+            } else if (this->map[y][x] == 6) {
+                this->mapSpritesID[y][x].setType(SPRITE);
+                this->mapSpritesID[y][x].addImage("pacman/resources/turn_up_left.png");
+            } else if (this->map[y][x] == 7) {
+                this->mapSpritesID[y][x].setType(SPRITE);
+                this->mapSpritesID[y][x].addImage("pacman/resources/turn_down_right.png");
+            } else if (this->map[y][x] == 8) {
+                this->mapSpritesID[y][x].setType(SPRITE);
+                this->mapSpritesID[y][x].addImage("pacman/resources/turn_down_left.png");
             } else if (this->map[y][x] == 2) {
                 this->mapSpritesID[y][x].setType(SPRITE);
                 this->mapSpritesID[y][x].addImage("pacman/resources/small_coin.png");
@@ -130,13 +136,12 @@ void Pacman::runGame()
 
     for (size_t y = 0; y < this->map.size(); y++) {
         for (size_t x = 0; x < this->map[y].size(); x++) {
-            std::cout << x << " " << y << std::endl;
             this->mapSpritesID[y][x].draw();
         }
     }
 }
 
-extern "C" std::shared_ptr<Pacman> create_object()
-{
-    return std::make_shared<Pacman>();
-}
+// extern "C" std::shared_ptr<Pacman> create_object()
+// {
+//     return std::make_shared<Pacman>();
+// }
