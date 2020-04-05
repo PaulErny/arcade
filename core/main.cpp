d32a0db5e5cab8169c473657b9030f48e898eddf
victor <victor.maurin@epitech.eu>
2020-04-05 20:43:18 +0200

    main.cpp

--- core/main.cpp
+++ core/main.cpp
@@ -9,9 +9,11 @@
 #include "../lib/ILibs.hpp"
 #include "Core.hpp"
 
-int main(int ac, char *av[])
+int main(int ac, char *av[], char *env[])
 {
     try {
+        if (!env)
+            throw "no env";
         if (ac != 2)
             throw "Arcade accept 2 arguments";
         srand(time(0));
